3ea2fc561a60bfd9093388b253c7f6276a1df264
Reuven Lazarus <rlazarus@alumni.reed.edu>
2018-11-10 12:04:15 -0500

    Barebones IRC and Twitch connections.

---
+++ irc_connection.py
@@ -0,0 +1,57 @@
+import sys
+from typing import Callable, Optional
+
+from irc import client
+
+import bot
+import custom
+import hello
+import logging
+import roulette
+
+
+class IrcConnection(bot.Connection, client.SimpleIRCClient):
+    def __init__(self, host: str, port: int, nickname: str, channel: str,
+                 password: Optional[str] = None) -> None:
+        super(IrcConnection, self).__init__()
+        self.host = host
+        self.port = port
+        self.nickname = nickname
+        self.channel = channel
+        self.password = password
+        self.callback: Callable[[bot.Message], None] = None
+
+    # bot.Connection overrides:
+
+    def say(self, text: str) -> None:
+        self.connection.privmsg(self.channel, text)
+
+    def run(self, callback: Callable[[bot.Message], None]) -> None:
+        self.callback = callback
+        self.connect(self.host, self.port, self.nickname, self.password)
+        self.start()
+
+    # client.SimpleIRCClient overrides:
+
+    def on_welcome(self, connection: client.ServerConnection,
+                   _: client.Event) -> None:
+        connection.join(self.channel)
+
+    def on_pubmsg(self, _: client.ServerConnection,
+                  event: client.Event) -> None:
+        self.callback(
+            bot.Message(event.source.nick, event.arguments[0], self.say))
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger(client.__name__)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+
+    conn = IrcConnection("irc.foonetic.net", 6667, "impbot", "#shrdlutesting")
+    handlers = [
+        custom.CustomCommandHandler(),
+        hello.HelloHandler(),
+        roulette.RouletteHandler(),
+    ]
+    bot.Bot("bot", [conn], handlers).run()
---
+++ twitch.py
@@ -0,0 +1,34 @@
+import logging
+import sys
+
+from irc import client
+
+import bot
+import custom
+import hello
+import irc_connection
+import roulette
+import secret
+
+
+class TwitchConnection(irc_connection.IrcConnection):
+    def __init__(self, bot_username: str, oauth_token: str,
+                 streamer_username: str) -> None:
+        if not oauth_token.startswith("oauth:"):
+            oauth_token = "oauth:" + oauth_token
+        super().__init__("irc.chat.twitch.tv", 6667, bot_username.lower(),
+                         "#" + streamer_username.lower(), password=oauth_token)
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger(client.__name__)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+
+    conn = TwitchConnection("BotAltBTW", secret.BOTALTBTW_OAUTH, "Shrdluuu")
+    handlers = [
+        custom.CustomCommandHandler(),
+        hello.HelloHandler(),
+        roulette.RouletteHandler(),
+    ]
+    bot.Bot("bot", [conn], handlers).run()
