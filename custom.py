26052753c6c9e8ed4d3b411f4f798a9b0ac618b5
Reuven Lazarus <rlazarus@alumni.reed.edu>
2018-11-17 15:35:11 -0500

    CustomCommandHandler's commands property accidentally shadowed CommandHandler's

--- custom.py
+++ custom.py
@@ -22,14 +22,14 @@ class Command(object):
 class CustomCommandHandler(command.CommandHandler):
     def __init__(self):
         super().__init__()
-        self.commands: Dict[str, Command] = {}
+        self.custom_commands: Dict[str, Command] = {}
 
     def check(self, message: bot.Message) -> bool:
         if super().check(message):
             return True
         if not message.text.startswith("!"):
             return False
-        return normalize(message.text.split(" ", 1)[0]) in self.commands
+        return normalize(message.text.split(" ", 1)[0]) in self.custom_commands
 
     def run(self, message: bot.Message) -> Optional[str]:
         # If CommandHandler's check() passes, this is a built-in like !addcom,
@@ -42,7 +42,7 @@ class CustomCommandHandler(command.CommandHandler):
         else:
             name, args = message.text, ""
         name = normalize(name)
-        c = self.commands[name]
+        c = self.custom_commands[name]
         if not c.cooldowns.fire(message.username):
             return None
         c.count += 1
@@ -50,27 +50,27 @@ class CustomCommandHandler(command.CommandHandler):
 
     def run_addcom(self, name: str, text: str) -> str:
         name = normalize(name)
-        if name in self.commands:
+        if name in self.custom_commands:
             raise bot.UserError(f"!{name} already exists.")
         if hasattr(self, "run_" + name):
             raise bot.UserError(f"Can't use !{name} for a command.")
-        self.commands[name] = Command(name, text)
+        self.custom_commands[name] = Command(name, text)
         return f"Added !{name}."
 
     def run_editcom(self, name: str, text: str) -> str:
         name = normalize(name)
-        if name in self.commands:
-            self.commands[name].response = text
+        if name in self.custom_commands:
+            self.custom_commands[name].response = text
             return f"Edited !{name}."
         else:
-            self.commands[name] = Command(name, text)
+            self.custom_commands[name] = Command(name, text)
             return f"!{name} didn't exist; added it."
 
     def run_delcom(self, name: str) -> str:
         name = normalize(name)
-        if name not in self.commands:
+        if name not in self.custom_commands:
             raise bot.UserError(f"!{name} doesn't exist.")
-        del self.commands[name]
+        del self.custom_commands[name]
         return f"Deleted !{name}."
 
     def run_resetcount(self, args: str) -> str:
@@ -88,7 +88,7 @@ class CustomCommandHandler(command.CommandHandler):
         else:
             name, count = args, 0
         name = normalize(name)
-        if name not in self.commands:
+        if name not in self.custom_commands:
             raise bot.UserError(f"!{name} doesn't exist")
-        self.commands[name].count = count
+        self.custom_commands[name].count = count
         return f"Reset !{name} counter to {count}."
