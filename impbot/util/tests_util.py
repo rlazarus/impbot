f4382abc0b64f3087ee41849dc135411cd6425af
Reuven Lazarus <rlazarus@alumni.reed.edu>
2019-09-08 17:31:14 -0400

    Whoops, fix a variable naming conflict

--- impbot/util/tests_util.py
+++ impbot/util/tests_util.py
@@ -10,13 +10,13 @@ from impbot.core import data
 class HandlerTest(unittest.TestCase):
     def setUp(self):
         super().setUp()
-        self.conn: base.ChatConnection = mock.Mock()
+        self.reply_conn: base.ChatConnection = mock.Mock()
         self.handler: base.Handler[base.Message] = None
 
     def _message(self, input: str, user: Optional[base.User] = None):
         if not user:
             user = base.User("username")
-        return base.Message(self.conn, user, input)
+        return base.Message(self.reply_conn, user, input)
 
     def assert_no_trigger(self, input: str) -> None:
         self.assertFalse(self.handler.check(self._message(input)))
