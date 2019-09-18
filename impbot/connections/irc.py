edc411031127d38bdecfe30eaf28012b6c6c5395
Reuven Lazarus <rlazarus@alumni.reed.edu>
2019-09-18 12:39:09 -0400

    IRC: Reconnect if we disconnect unexpectedly.

--- impbot/connections/irc.py
+++ impbot/connections/irc.py
@@ -1,5 +1,7 @@
 import logging
 import sys
+import threading
+import time
 from typing import Optional, List
 
 from irc import client
@@ -9,6 +11,8 @@ from impbot.core import base
 from impbot.handlers import custom
 from impbot.handlers import hello
 from impbot.handlers import roulette
+
+logger = logging.getLogger(__name__)
 
 
 class IrcConnection(base.ChatConnection, client.SimpleIRCClient):
@@ -22,6 +26,8 @@ class IrcConnection(base.ChatConnection, client.SimpleIRCClient):
         self.channel = channel
         self.password = password
         self.capabilities = capabilities if capabilities is not None else []
+        self.shutdown_event = threading.Event()
+        self.expect_disconnection = False
         self.on_event: Optional[base.EventCallback] = None
 
     # bot.Connection overrides:
@@ -31,13 +37,21 @@ class IrcConnection(base.ChatConnection, client.SimpleIRCClient):
 
     def run(self, on_event: base.EventCallback) -> None:
         self.on_event = on_event
-        self.connect(self.host, self.port, self.nickname, self.password)
-        # SimpleIRCClient.start() never returns even after disconnection, so
-        # instead of calling into it, we run this loop ourselves.
-        while self.connection.connected:
-            self.reactor.process_once(0.2)
+        while not self.shutdown_event.is_set():
+            logger.info("Connecting...")
+            self.connect(self.host, self.port, self.nickname, self.password)
+            # SimpleIRCClient.start() never returns even after disconnection, so
+            # instead of calling into it, we run this loop ourselves.
+            while self.connection.connected:
+                self.reactor.process_once(0.2)
+            logger.info("Disconnected.")
+            if not self.expect_disconnection:
+                time.sleep(3)  # It's, uh, exponential backoff with a base of 1.
+            self.expect_disconnection = False
 
     def shutdown(self) -> None:
+        self.expect_disconnection = True
+        self.shutdown_event.set()
         self.connection.close()
 
     # client.SimpleIRCClient overrides:
@@ -58,18 +72,3 @@ class IrcConnection(base.ChatConnection, client.SimpleIRCClient):
 
     def _user(self, event: client.Event) -> base.User:
         return base.User(event.source.nick)
-
-
-if __name__ == "__main__":
-    logger = logging.getLogger(client.__name__)
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(logging.StreamHandler(sys.stdout))
-
-    conn = IrcConnection("irc.foonetic.net", 6667, "impbot", "#shrdlutesting")
-    handlers = [
-        custom.CustomCommandHandler(),
-        hello.HelloHandler(),
-        roulette.RouletteHandler(),
-    ]
-    b = bot.Bot("impbot.sqlite", [conn], handlers)
-    b.main()
