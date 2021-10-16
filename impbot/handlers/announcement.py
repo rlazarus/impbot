c150f3ab2d51b1732ed7c36e46d53db169020f48
Reuven Lazarus <rlazarus@alumni.reed.edu>
2021-10-16 14:38:15 -0700

    announcement: Fetch interval from DB

--- impbot/handlers/announcement.py
+++ impbot/handlers/announcement.py
@@ -10,8 +10,6 @@ from impbot.core import base
 from impbot.handlers import command
 from impbot.util import twitch_util
 
-INTERVAL = timedelta(hours=2)
-
 logger = logging.getLogger(__name__)
 
 
@@ -25,13 +23,14 @@ class AnnouncementHandler(command.CommandHandler):
         self.util = util
 
     def startup(self) -> None:
+        interval = self._interval()
         try:
             last_announce = self.data.get("last_announce")
         except KeyError:
             last_announce_time = datetime.fromtimestamp(0, tz=timezone.utc)
         else:
             last_announce_time = dateutil.parser.isoparse(last_announce)
-        if datetime.now(timezone.utc) - last_announce_time > INTERVAL:
+        if datetime.now(timezone.utc) - last_announce_time > interval:
             # The announcement is overdue. This happens if the bot was offline
             # when it should have fired. (It also happens when the bot is
             # started up for the first time, but then we just announce the empty
@@ -40,13 +39,16 @@ class AnnouncementHandler(command.CommandHandler):
             last_announce_time = datetime.now(timezone.utc)
         # Wait out the rest of the interval, then announce and start the
         # repeating timer.
-        wait = last_announce_time + INTERVAL - datetime.now(timezone.utc)
+        wait = last_announce_time + interval - datetime.now(timezone.utc)
 
         def run():
             self.announce()
-            self.timer_conn.start_repeating(INTERVAL, self.announce)
+            self.timer_conn.start_repeating(self._interval(), self.announce)
 
         threading.Timer(wait.total_seconds(), run).start()
+
+    def _interval(self) -> timedelta:
+        return timedelta(minutes=int(self.data.get("interval", default=30)))
 
     def announce(self):
         data = self.util.get_stream_data(username=self.util.streamer_username)
