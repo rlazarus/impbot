719592b11b1a9b7611588fd7a60a0620b7b83893
Reuven Lazarus <rlazarus@alumni.reed.edu>
2021-10-24 15:26:21 -0700

    twitch_editor: Print current info instead of an error, when unprivileged.

--- impbot/handlers/twitch_editor.py
+++ impbot/handlers/twitch_editor.py
@@ -17,21 +17,19 @@ class TwitchEditorHandler(command.CommandHandler):
         self.twitch_util.oauth.maybe_authorize()
 
     def run_title(self, message: base.Message, title: Optional[str]):
-        if not title:
+        if not title or not (message.user.moderator or message.user.admin):
             data = self.twitch_util.get_stream_data(username=self.twitch_util.streamer_username)
             if data == twitch_util.OFFLINE:
                 return 'Stream is offline.'
             current_title = data['title']
             return f'Current title: {current_title}'
-        if not (message.user.moderator or message.user.admin):
-            raise base.UserError("You can't do that.")
         channel_id = self.twitch_util.get_channel_id(self.twitch_util.streamer_username)
         self.twitch_util.helix_patch(
             'channels', params={'broadcaster_id': channel_id}, json={'title': title})
         return 'Done!'
 
     def run_game(self, message: base.Message, game: Optional[str]):
-        if not game:
+        if not game or not (message.user.moderator or message.user.admin):
             data = self.twitch_util.get_stream_data(username=self.twitch_util.streamer_username)
             if data == twitch_util.OFFLINE:
                 return 'Stream is offline.'
@@ -39,8 +37,6 @@ class TwitchEditorHandler(command.CommandHandler):
             game_id = int(data['game_id'])
             current_game = self.twitch_util.game_name(game_id)
             return f'Current game: {current_game}'
-        if not (message.user.moderator or message.user.admin):
-            raise base.UserError("You can't do that.")
         channel_id = self.twitch_util.get_channel_id(self.twitch_util.streamer_username)
         game_id = self.twitch_util.game_id(game)  # Raises UserError if the game doesn't exist.
         self.twitch_util.helix_patch(
